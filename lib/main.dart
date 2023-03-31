5353a5da99fb03ebf23761001f2ff6b2486acaf6
Sigumaa <66453922+Sigumaa@users.noreply.github.com>
2023-03-31 10:28:40 +0900

    add ProfileImage, SkillSet, LinkIconsRow widgets to HomePage class

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
 void main() {
@@ -34,23 +33,10 @@ class HomePage extends StatelessWidget {
               SizedBox(
                 height: 32,
               ),
-              Container(
-                width: 256,
-                height: 256,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                    color: const Color(0xff506482),
-                    width: 2,
-                  ),
-                  image: const DecorationImage(
-                    image: NetworkImage('https://avatars.githubusercontent.com/u/66453922'),
-                  ),
-                ),
-              ),
+              ProfileImage(),
               Text(
                 'しゆい',
-                style: Theme.of(context).textTheme.headlineMedium,
+                style: Theme.of(context).textTheme.headlineSmall,
               ),
               Text(
                 'Student, Go Lover',
@@ -59,73 +45,110 @@ class HomePage extends StatelessWidget {
               SizedBox(
                 height: 32,
               ),
-              Text(
-                'Skills',
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              Text(
-                'Go,TyperScript',
-              ),
+              SkillSet(skills: ['Go','TypeScript']),
               SizedBox(
                 height: 32,
               ),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      print('Twitter');
-                    },
-                    child: SvgPicture.asset(
-                      'assets/twitter.svg',
-                      colorFilter: ColorFilter.mode(
-                        const Color(0xff506482),
-                        BlendMode.srcIn,
-                      ),
-                      width: 32,
-                      height: 32,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 16,
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      launchUrlString('https://github.com/Sigumaa');
-                    },
-                    child: SvgPicture.asset(
-                      'assets/github.svg',
-                      colorFilter: ColorFilter.mode(
-                        const Color(0xff506482),
-                        BlendMode.srcIn,
-                      ),
-                      width: 32,
-                      height: 32,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 16,
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      launchUrlString('https://www.shiyui.dev/');
-                    },
-                    child: SvgPicture.asset(
-                      'assets/user-solid.svg',
-                      colorFilter: ColorFilter.mode(
-                        const Color(0xff506482),
-                        BlendMode.srcIn,
-                      ),
-                      width: 32,
-                      height: 32,
-                    ),
-                  ),
-                ],
-              ),
+              LinkIconsRow(),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class ProfileImage extends StatelessWidget {
+  const ProfileImage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 200,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: const Color(0xff506482),
+          width: 2,
+        ),
+        image: const DecorationImage(
+          image: NetworkImage('https://avatars.githubusercontent.com/u/66453922'),
+        ),
+      ),
+    );
+  }
+}
+
+class SkillSet extends StatelessWidget {
+  final List<String> skills;
+
+  const SkillSet({Key? key, required this.skills}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'Skills',
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        const SizedBox(height: 8),
+        Column(
+          children: skills.map((skill) => Text(skill)).toList()),
+      ],
+    );
+  }
+}
+
+class LinkIcon extends StatelessWidget {
+  final String link;
+  final String iconPath;
+
+  const LinkIcon({Key? key, required this.link, required this.iconPath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        launchUrlString(link);
+      },
+      child: SvgPicture.asset(
+        iconPath,
+        colorFilter: ColorFilter.mode(
+          const Color(0xff506482),
+          BlendMode.srcIn,
+        ),
+        width: 32,
+        height: 32,
+      ),
+    );
+  }
+}
+
+class LinkIconsRow extends StatelessWidget {
+  const LinkIconsRow({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        LinkIcon(
+          link: 'https://twitter.com/',
+          iconPath: 'assets/twitter.svg',
+        ),
+        const SizedBox(width: 16),
+        LinkIcon(
+          link: 'https://github.com/Sigumaa',
+          iconPath: 'assets/github.svg',
+        ),
+        const SizedBox(width: 16),
+        LinkIcon(
+          link: 'https://www.shiyui.dev/',
+          iconPath: 'assets/user-solid.svg',
+        ),
+      ],
     );
   }
 }
